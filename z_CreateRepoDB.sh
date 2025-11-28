a5017268e6ec293febb4ea4fb449063e82cb4e34
TushantVerma <TushantVerma@users.noreply.github.com>
2025-11-28 21:41:15 +0530

    adjustment in text

--- z_CreateRepoDB.sh
+++ z_CreateRepoDB.sh
@@ -20,10 +20,10 @@ rm Bharat-OS.files      # delete symlink
 rm Bharat-OS.files.sig  # delete symlink .sig
 
 
-mv Bharat-OS.db.tar.zst     Bharat-OS.db            # using actual file instead of symlink
-mv Bharat-OS.db.tar.zst.sig Bharat-OS.db.sig        # using actual file instead of symlink .sig
-mv Bharat-OS.files.tar.zst Bharat-OS.files  # using actual file instead of symlink
-mv Bharat-OS.files.tar.zst.sig Bharat-OS.files.sig  # using actual file instead of symlink .sig
+mv Bharat-OS.db.tar.zst         Bharat-OS.db            # using actual file instead of symlink
+mv Bharat-OS.db.tar.zst.sig     Bharat-OS.db.sig        # using actual file instead of symlink .sig
+mv Bharat-OS.files.tar.zst      Bharat-OS.files         # using actual file instead of symlink
+mv Bharat-OS.files.tar.zst.sig  Bharat-OS.files.sig     # using actual file instead of symlink .sig
 
 popd  ## exit from directory #-----------#
 echo "####################################"
